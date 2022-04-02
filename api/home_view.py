65cb194c309bb17f3e9ea8b6116867a1b5a3d779
Kostas <Ziovas>
2022-04-03 00:43:31 +0300

    Added basic home page, resume_app class and created structure for the entire project

---
+++ api/home_view.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+home_bp = Blueprint("home_view", __name__)
+
+
+@home_bp.route("/")
+def home():
+    return "Welcome to Resume Web Page"
--- resume_app.py
+++ resume_app.py
@@ -1,6 +1,10 @@
+import json
 from flask import Flask
 from logging import Logger
 from injector import inject, singleton
+from core import ConfigService
+from api import home_bp
+from shared import APP_NAME
 
 
 
@@ -10,22 +14,12 @@ class ResumeApp:
     def __init__(
         self,
         logger: Logger,
-        mongo_engine: MongoEngine,
         config_service: ConfigService,
-        pets_view_manager: PetsController,
-        user_controller: UserController,
-        access_controller: AcessController,
-        product_controller: ProductController,
     ) -> None:
         self.logger = logger
-        self.mongo_engine = mongo_engine
         self.config_service = config_service
-        self.pets_view_manager = pets_view_manager
-        self.user_controller = user_controller
-        self.access_controller = access_controller
-        self.product_controller = product_controller
 
-    def create_app(self, name: str = "PetStore"):
+    def create_app(self, name: str = APP_NAME):
         app = Flask(name)
         try:
             self.config_service.load()
@@ -38,23 +32,11 @@ class ResumeApp:
             settings = json.load(f)
 
         app.config.update(settings)
-        # app.config['MONGODB_SETTINGS'] = {
-        #    'host':'mongodb://petstore_mongo/petstore'
-        # }
 
-        self.mongo_engine.init_app(app)
 
         # Initiliaze configuration and URL rules for all views
-        self.pets_view_manager.configure()
-        self.user_controller.configure()
-        self.access_controller.configure()
-        self.product_controller.configure()
 
         # Register blueprints
         app.register_blueprint(home_bp)
-        app.register_blueprint(self.pets_view_manager.pets_bp)
-        app.register_blueprint(self.user_controller.user_bp)
-        app.register_blueprint(self.access_controller.access_bp)
-        app.register_blueprint(self.product_controller.product_bp)
 
         return app
