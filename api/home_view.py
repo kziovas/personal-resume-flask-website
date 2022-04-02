50a5bca833545c46eb8724620d52f35559b40f2b
Kostas <Ziovas>
2022-04-03 02:31:23 +0300

    Added first html template for home page

--- api/home_view.py
+++ api/home_view.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home_bp = Blueprint("home_view", __name__)
 
 
 @home_bp.route("/")
-def home():
-    return "Welcome to Resume Web Page"
+def index():
+    return render_template('index.html')
