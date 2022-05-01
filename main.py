4594f76aa1b90245bc62f895bd220767ae02f8f2
Kostas <Ziovas>
2022-05-01 14:22:19 +0300

    Removed app.run()

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     resume_app = injector.get(ResumeApp)
     app = resume_app.create_app()
     app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
- 
-    app.run(host=resume_app.config_service.host,port=resume_app.config_service.app_port)
+
+    # app.run(host=resume_app.config_service.host,port=resume_app.config_service.app_port)
 
 
 if __name__ == "__main__":
